ad3c680bc8e993106a4f1373ff94e3f22e3c8b92
joao roberto <jrgarske5@gmail.com>
2024-04-21 18:25:10 -0300

    endpoint para retornar os posts de um usuario

--- src/main/java/com/joaoroberto/workshopmongo/resources/UserResource.java
+++ src/main/java/com/joaoroberto/workshopmongo/resources/UserResource.java
@@ -1,14 +1,11 @@
 package com.joaoroberto.workshopmongo.resources;
 
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.objenesis.instantiator.basic.NewInstanceInstantiator;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,11 +14,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.joaoroberto.workshopmongo.doman.Post;
 import com.joaoroberto.workshopmongo.doman.User;
 import com.joaoroberto.workshopmongo.dto.UserDTO;
 import com.joaoroberto.workshopmongo.services.UserService;
-
-import jakarta.servlet.Servlet;
 
 @RestController
 @RequestMapping(value="/users")
@@ -73,6 +69,13 @@ public class UserResource {
 		
 		return ResponseEntity.noContent().build();
 		
+	}
+	
+	@RequestMapping(value="/{id}/posts",method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 
 }
