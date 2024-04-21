299aa17f0c9836a2eac616d67f3e6763a8c46f3b
joao roberto <jrgarske5@gmail.com>
2024-04-21 01:36:34 -0300

    primeiro commit projeto criado

---
+++ src/test/java/com/joaoroberto/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaoroberto.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
